1e743476c8bc693882bbc4becf5ecb137c54e9e6
vgabriel10 <vittorgabriell420@gmail.com>
2024-12-04 22:49:20 -0300

    first commit

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        // POST api/users
+        [HttpPost]
+        public IActionResult Post(CreateUserInputModel model)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}/skills")]
+        public IActionResult PostSkills(UserSkillsInputModel model)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/profile-picture")]
+        public IActionResult PostProfilePicture(IFormFile file)
+        {
+            var description = $"FIle: {file.FileName}, Size: {file.Length}";
+
+            // Processar a imagem
+
+            return Ok(description);
+        }
+    }
+}
