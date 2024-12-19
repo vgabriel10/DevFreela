3cdcde39d8bf9bd6184026d29f297ad34a4efe11
vgabriel10 <vittorgabriell420@gmail.com>
2024-12-18 21:25:16 -0300

    finalizando commands

---
+++ DevFreela.Application/Commands/StartProject/StartProjectHandler.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Application.Commands.CompleteProject;
+using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Commands.StartProject
+{
+    public class StartProjectHandler : IRequestHandler<StartProjectCommand, ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public StartProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ResultViewModel> Handle(StartProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _context.Projects.SingleOrDefaultAsync(p => p.Id == request.Id);
+
+            if (project is null)
+                return ResultViewModel.Error("Projeto não existe!");
+
+            project.Start();
+            _context.Update(project);
+            await _context.SaveChangesAsync();
+
+            return ResultViewModel.Sucess();
+        }
+    }
+}
