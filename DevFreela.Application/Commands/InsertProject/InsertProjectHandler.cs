e1f7fd47f22ea3fc666be2096c89f70a9f284cd0
vgabriel10 <vittorgabriell420@gmail.com>
2024-12-21 12:25:24 -0300

    Adicionando notificacoes com o mediator

--- DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
+++ DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreela.Application.Models;
+using DevFreela.Application.Notification.ProjectCreated;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using System;
@@ -12,10 +13,12 @@ namespace DevFreela.Application.Commands.InsertProject
     public class InsertProjectHandler : IRequestHandler<InsertProjectCommand,ResultViewModel<int>>
     {
         private readonly DevFreelaDbContext _context;
+        private readonly IMediator _mediator;
 
-        public InsertProjectHandler(DevFreelaDbContext context)
+        public InsertProjectHandler(DevFreelaDbContext context, IMediator mediator)
         {
             _context = context;
+            _mediator = mediator;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
@@ -23,6 +26,9 @@ namespace DevFreela.Application.Commands.InsertProject
             var project = request.ToEntity();
             await _context.Projects.AddAsync(project);
             await _context.SaveChangesAsync();
+
+            var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
+            await _mediator.Publish(projectCreated);
 
             return ResultViewModel<int>.Sucess(project.Id);
         }
