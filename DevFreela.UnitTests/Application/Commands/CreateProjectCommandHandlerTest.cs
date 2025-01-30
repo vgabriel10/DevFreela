ebf8f4dabc6c7a6402b2f2905fa1bf0cf42989be
vgabriel10 <vittorgabriell420@gmail.com>
2025-01-29 21:50:19 -0300

    Adicionando teste unitario para CreateProject

---
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTest.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.Application.Commands.InsertProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    
+    public class CreateProjectCommandHandlerTest
+    {
+        [Fact]
+        public async Task InputDataIsOK_Executed_ReturnProjectId()
+        {
+            // Arrange
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            var mediatorMock = new Mock<IMediator>();
+            var createdProjectCommand = new InsertProjectCommand
+            {
+                IdClient = 1,
+                IdFreelancer = 2,
+                Title = "Sistema de estacionamento",
+                Description = "Description",
+                TotalCost = 10000
+            };
+
+            var createdProjectCommandHandler = new InsertProjectHandler(projectRepositoryMock.Object, mediatorMock.Object);
+
+            // Act
+            var id = await createdProjectCommandHandler.Handle(createdProjectCommand,new CancellationToken());
+
+            // Assert
+            Assert.True(id.Data >= 0);
+
+            projectRepositoryMock.Verify(pr => pr.Add(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
