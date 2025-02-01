8b230412851d499b00ce95b562518d3eaa809195
vgabriel10 <vittorgabriell420@gmail.com>
2025-01-31 23:03:11 -0300

    criando UserQuery

---
+++ DevFreela.Application/Queries/UserQuerties/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿
+
+using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Queries.UserQuerties.GetUserById
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, ResultViewModel<UserViewModel>>
+    {
+        private readonly IUserRepository _repository;
+
+        public GetUserByIdHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _repository.GetByIdAsync(request.Id);
+            if (user is null)
+                return ResultViewModel<UserViewModel>.Error("Usuário não encontrado");
+
+            var model = UserViewModel.FromEntity(user);
+            return ResultViewModel<UserViewModel>.Sucess(model);
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/UserQuerties/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿
+
+using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Queries.UserQuerties.GetUserById
+{
+    public class GetUserByIdQuery : IRequest<ResultViewModel<UserViewModel>>
+    {
+        public GetUserByIdQuery(int id) 
+        { 
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
