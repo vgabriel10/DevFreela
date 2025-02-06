2aab3e19e1489a826a5d2adf0a69c3fa17aeec40
vgabriel10 <vittorgabriell420@gmail.com>
2025-02-05 21:25:27 -0300

    criando paymentService e configurando uma URL para requisicoes http no appSettings.json

---
+++ DevFreela.Core/Services/IPaymentlService.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Core.Services
+{
+    public interface IPaymentlService
+    {
+        Task<bool> ProcessPayament(PaymentInfo paymentInfoInputModel);
+    }
+}
---
+++ DevFreela.Infrastructure/Payments/PaymentService.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace DevFreela.Infrastructure.Payments
+{
+    public class PaymentService : IPaymentlService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _paymentsBaseUrl;
+
+        public PaymentService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _paymentsBaseUrl = configuration.GetSection("Services:Payments").Value;
+        }
+
+        public Task<bool> ProcessPayament(PaymentInfo paymentInfoInputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
