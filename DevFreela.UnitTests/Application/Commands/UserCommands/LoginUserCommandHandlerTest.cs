60bbd18caa9857c30a3c11fc82eaf508548a3c6d
vgabriel10 <vittorgabriell420@gmail.com>
2025-03-10 23:10:51 -0300

    Iniciando criacao de teste unitario para login

---
+++ DevFreela.UnitTests/Application/Commands/UserCommands/LoginUserCommandHandlerTest.cs
@@ -0,0 +1,49 @@
+﻿
+
+using DevFreela.Application.Commands.UserCommands.LoginUser;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Commands.UserCommands
+{
+    public class LoginUserCommandHandlerTest
+    {
+        [Fact]
+        public async Task GivenUserWithValidCredentials_WhenAuthenticate_ThenReturnToken()
+        {
+            // Arrange
+            var email = "teste@gmail.com";
+            var password = "123456";
+            var passwordHash = "passwordHash";
+            var token = "token";
+            var role = "client";
+            var user = new User("teste", email, new DateTime(2000, 01, 01), password, role);
+
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var authServiceMock = new Mock<IAuthService>();
+
+            authServiceMock.Setup(a => a.ComputeSha256Hash(password)).Returns(passwordHash);
+            userRepositoryMock.Setup(ur => ur.GetUserByEmailAndPasswordAsync(email, passwordHash)).Returns(Task.FromResult(user));
+            authServiceMock.Setup(a => a.GenerateJwtToken(email, role)).Returns(token);
+
+            var loginUserCommand = new LoginUserCommand
+            {
+                Email = email,
+                Password = password
+            };
+
+            var loginUserCommandHandler = new LoginUserHandler(authServiceMock.Object, userRepositoryMock.Object);
+
+            // Act
+            var result = await loginUserCommandHandler.Handle(loginUserCommand, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equals(result.Email, email);
+
+
+        }
+    }
+}
